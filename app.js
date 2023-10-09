e9950fc07ab08566daaaef09f7c4a6e504db74b2
CMF21 <cristianmauriciofalcon@gmail.com>
2023-10-08 21:10:36 -0300

    modifico app para probar el error

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log('gatito mimoso');
 //DEFINO UNA VARIABLE
 let variable1 = 'No se pierdan el debante presidenical a las 21hs ARGENTINA'
 console.log(variable1);
+const n = 'probando el error  '
