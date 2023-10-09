55b7c34687d1bdf4412191c697242d12695dedb2
EBN10 <basilemir@gmail.com>
2023-10-08 21:18:36 -0300

    sacando el link

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log('gatito mimoso');
 //DEFINO UNA VARIABLE
-let variable1 = 'No se pierdan el debante presidenical a las 21hs ARGENTINA'
+let variable1 = 'No se pierdan el debante presidenical a las 21hs ARGENTINAA'
 console.log(variable1);
